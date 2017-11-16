332bfd125df87955a947becffa08748b6e9b1805
longdq <longdq2902@gmail.com>
2017-11-16 08:12:32 +0700

    Commit bai 11

--- TeduShop.Data/TeduShopDbContext.cs
+++ TeduShop.Data/TeduShopDbContext.cs
@@ -31,6 +31,8 @@ namespace TeduShop.Data
 
         public DbSet<VisitorStatistic> VisitorStatistics { set; get; }
 
+        public DbSet<Error> Errors { set; get; }
+
 
         protected override void OnModelCreating(DbModelBuilder builder)
         {
---
+++ TeduShop.Service/ErrorService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Service
+{
+    public interface IErrorMessage
+    {
+        void Create(Error error);
+        void Save();
+    }
+
+    public class ErrorService : IErrorMessage
+    {
+        IErrorRepository _errorRepository;
+        IUnitOfWork _unitOfWork;
+
+        public ErrorService(IErrorRepository errorRepository, IUnitOfWork unitOfWork)
+        {
+            this._errorRepository = errorRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public void Create(Error error)
+        {
+             _errorRepository.Add(error);
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+    }
+}
